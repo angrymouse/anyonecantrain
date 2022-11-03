8a17b9feb5565e224104e9d8c876314a788c7024
Sh1Tt <info@mauricestolk.nl>
2022-11-03 07:38:49 +0100

    Added loader + message

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,19 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
- modules: ['@nuxtjs/tailwindcss'],
-   target: "static",
-   mode: "spa",
- ssr:false,
- colorMode:{
-    dataValue:"luxury"
- },
- app: {
+    modules: ['@nuxtjs/tailwindcss'],
+    target: "static",
+    mode: "spa",
+    ssr:false,
+    css: [
+        "~/assets/style/dna.css"
+    ],
+    colorMode:{
+        dataValue:"luxury"
+    },
+    app: {
         head: {
-        htmlAttrs:{"data-theme": "halloween",}
+            htmlAttrs:{"data-theme": "halloween",}
+        }
     }
-   },
-
-
-})
+});
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,76 @@
 <template>
+    <head>
+      <link rel="stylesheet" href="~/assets/style/dna.css" />
+    </head>
     <div class="h-full w-full flex flex-col items-center justify-center">
+        <div id="loader" class="anchor">
+            <div class="dna">
+                <div class="particles" id="blue">
+                    <div class="particle blue" id="p_b__00"></div>
+                    <div class="particle blue" id="p_b__01"></div>
+                    <div class="particle blue" id="p_b__02"></div>
+                    <div class="particle blue" id="p_b__03"></div>
+                    <div class="particle blue" id="p_b__04"></div>
+                    <div class="particle blue" id="p_b__05"></div>
+                    <div class="particle blue" id="p_b__06"></div>
+                    <div class="particle blue" id="p_b__07"></div>
+                    <div class="particle blue" id="p_b__08"></div>
+                    <div class="particle blue" id="p_b__09"></div>
+                </div>
+                <div class="particles" id="red">
+                    <div class="particle red" id="p_r__00"></div>
+                    <div class="particle red" id="p_r__01"></div>
+                    <div class="particle red" id="p_r__02"></div>
+                    <div class="particle red" id="p_r__03"></div>
+                    <div class="particle red" id="p_r__04"></div>
+                    <div class="particle red" id="p_r__05"></div>
+                    <div class="particle red" id="p_r__06"></div>
+                    <div class="particle red" id="p_r__07"></div>
+                    <div class="particle red" id="p_r__08"></div>
+                    <div class="particle red" id="p_r__09"></div>
+                </div>
+                <div class="rods">
+                    <div class="rod" id="r__00"></div>
+                    <div class="rod" id="r__01"></div>
+                    <div class="rod" id="r__02"></div>
+                    <div class="rod" id="r__03"></div>
+                    <div class="rod" id="r__04"></div>
+                    <div class="rod" id="r__05"></div>
+                    <div class="rod" id="r__06"></div>
+                    <div class="rod" id="r__07"></div>
+                    <div class="rod" id="r__08"></div>
+                    <div class="rod" id="r__09"></div>
+                </div>
+            </div>
+            <!-- Display text -->
+            <div class="progress">
+                <img src="~assets/brain.svg" class="w-24 mt-6" />
+                <h2>
+                    Welcome to anyoneCanTrain!
+                </h2>
+                <p id="loader__progress">
+                    Please give me a couple of minutes<br />
+                    to get the data from arweave and<br />
+                    initialize the neural net with it..
+                </p>
+            </div>
+        </div>
         <img src="~assets/brain.svg" class="w-24 mt-6" />
-        <h1 class="text-3xl">Neural network that anyone can train</h1>
-        <p>Censorship resistant and permanent with <a href="https://arweave.org/" class="link link-primary">Arweave</a>,
+        <h1 class="text-3xl">
+            Neural network that anyone can train
+        </h1>
+        <p>
+            Censorship resistant and permanent with <a href="https://arweave.org/" class="link link-primary">Arweave</a>,
             powered by <a class="link link-primary" href="https://brain.js.org/">brain.js</a>
         </p>
-
-        <h2 class="text-2xl mt-4 text-center w-96 flex flex-row justify-center items-center">Ask it!
-
-
-
-
+        <h2 class="text-2xl mt-4 text-center w-96 flex flex-row justify-center items-center">
+            Ask it!
         </h2>
         <div class="form-control mt-2 w-full flex flex-row justify-center">
             <div class="input-group lg:w-2/4">
                 <input type="text" placeholder="E.g What's your name?" class="input input-bordered w-full"
-                    v-model="askInput" />
-                <button class="btn btn-square" @click.prevent="ask">
+                    v-model="question" />
+                <button class="btn btn-square" @click.prevent="questionHandler">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36" class="w-6 inline-block ">
                         <path fill="#BE1931"
                             d="M21 27c-1.657 0-3-1.343-3-3v-4c0-1.657 1.343-3 3-3 .603-.006 6-1 6-5 0-2-2-4-5-4-2.441 0-4 2-4 3 0 1.657-1.343 3-3 3s-3-1.343-3-3c0-4.878 4.58-9 10-9 8 0 11 5.982 11 11 0 4.145-2.277 7.313-6.413 8.92-.9.351-1.79.587-2.587.747V24c0 1.657-1.343 3-3 3z" />
@@ -28,13 +82,17 @@
                 </button>
             </div>
         </div>
-        <p class="bg-neutral text-primary w-3/4 lg:w-1/4 mt-2 p-2 rounded-md" v-if="reply">{{ reply }}</p>
-        <h2 class="text-2xl mt-4">Train it!</h2>
+        <p class="bg-neutral text-primary w-3/4 lg:w-1/4 mt-2 p-2 rounded-md" v-if="reply">
+            {{ reply }}
+        </p>
+        <h2 class="text-2xl mt-4">
+            Train it!
+        </h2>
         <div class="form-control mt-2 w-full flex flex-row justify-center">
             <div class="input-group lg:w-2/4">
                 <input type="text" placeholder="E.g Arweave is best storage protocol"
-                    class="input input-bordered w-full" v-model="trainingInput" />
-                <button class="btn btn-square" @click.prevent="train">
+                    class="input input-bordered w-full" v-model="traindata" />
+                <button class="btn btn-square" @click.prevent="trainHandler">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 36 36" class="h-6 w-6">
                         <path fill="#31373D" d="M24 14H12s-5 6-5 10 11 11 11 11 11-7 11-11-5-10-5-10z" />
                         <path fill="#292F33"
@@ -50,57 +108,95 @@
                 </button>
             </div>
         </div>
-        <p class="bg-neutral text-primary w-3/4 lg:w-1/4 mt-4 p-2 rounded-md " v-if="trainingResult">
-            Trained!&nbsp;Now&nbsp;he&nbsp;knows that&nbsp;"{{ trainingResult }}"
+        <p class="bg-neutral text-primary w-3/4 lg:w-1/4 mt-4 p-2 rounded-md " v-if="results">
+            Trained!&nbsp;Now&nbsp;he&nbsp;knows that&nbsp;"{{ results }}"
         </p>
     </div>
 </template>
 
 <script setup>
-import Arweave from "arweave"
-import ArDB from 'ardb';
-import * as brain from "brain.js";
-console.log(brain)
-let anyoneCanTrain = new brain.recurrent.LSTM();
-const arweaveState = useState("arweave", () => Arweave.init({
-    host: 'arweave.net',
-    port: 443,
-    protocol: 'https',
-    timeout: 60000,
-    logging: false,
-}));
-let arweave = arweaveState.value
-console.log(anyoneCanTrain)
-const ardbState = useState("ardb", () => new ArDB(arweave.value));
-let ardb = ardbState.value
-let reply = ref("")
-let askInput = ref("")
-let trainingInput = ref("")
-let trainingResult = ref(null)
-ardb.search("transactions").tag("Protocol-Name", "AnyoneCanTrain").exclude("anchor").findAll().then(async transactions => {
-    console.log(transactions)
-    transactions = await Promise.all(transactions.map(async transaction => {
-        return await fetch(`https://arweave.net/` + transaction.id).then(r => r.text())
-    }))
-    console.log(transactions)
-    await anyoneCanTrain.train(["My name is Ivan", "Your name... You didn't tell me your name! I'm neural network, not all-might AI.", ...transactions])
-    console.log(anyoneCanTrain.toJSON())
-})
-async function ask() {
-    reply.value = await anyoneCanTrain.run(askInput.value)
-    console.log(reply.value)
-}
-async function train() {
-    let transaction = await arweave.createTransaction({
-        data: trainingInput.value,
-    })
-    transaction.addTag('Content-Type', 'text/plain');
-    transaction.addTag('Protocol-Name', 'AnyoneCanTrain');
-    let clone = trainingInput.value
-    trainingInput.value = ""
-    await window.arweaveWallet.dispatch(transaction)
-    await anyoneCanTrain.train([clone]);
-    trainingResult.value = clone;
+    import Arweave from "arweave"
+    import ArDB from "ardb";
+    import * as brain from "brain.js";
+    
+    const removeLoader = () => {
+        const loader = document.querySelector(`#loader`);
+        loader.parentNode.removeChild(loader);
+    };
+    
+    let anyoneCanTrain = new brain.recurrent.LSTM();
 
-}
+    const arweaveState = useState("arweave", () => {
+        Arweave.init({
+            host: "arweave.net",
+            port: 443,
+            protocol: "https",
+            timeout: 60_000,
+            logging: false,
+        });
+    });
+    
+    const initialState = {
+        reply: "",
+        question: "",
+        traindata: "",
+        results: null
+    };
+    
+    let arweave = arweaveState.value;
+    
+    console.log(anyoneCanTrain);
+    
+    const ardbState = useState("ardb", () => new ArDB(arweave.value));
+    
+    let ardb = ardbState.value;
+    
+    let reply = ref(initialState.reply);
+    let question = ref(initialState.question);
+    let traindata = ref(initialState);
+    let results = ref(null);
+    
+    ardb.search("transactions")
+    .tag("Protocol-Name", "AnyoneCanTrain")
+    .exclude("anchor")
+    .findAll()
+    .then(async transactions => {
+        console.log(transactions);
+
+        transactions = await Promise.all(transactions.map(async transaction => {
+            const URL = `https://arweave.net/${transaction.id}`;
+            const r = await fetch(URL);
+            return await r.text();
+        }));
+
+        console.log(transactions);
+        
+        await anyoneCanTrain.train([
+            "My name is Ivan", 
+            "Your name... You didn't tell me your name! I'm neural network, not all-might AI.", 
+            ...transactions
+        ]);
+        
+        console.log(anyoneCanTrain.toJSON());
+
+        removeLoader();
+    });
+
+    const questionHandler = async () => {
+        reply.value = await anyoneCanTrain.run(question.value);
+        console.log(reply.value);
+    };
+    
+    const trainHandler = async () => {
+        let transaction = await arweave.createTransaction({
+            data: traindata.value,
+        });
+        transaction.addTag('Content-Type', 'text/plain');
+        transaction.addTag('Protocol-Name', 'AnyoneCanTrain');
+        let clone = traindata.value;
+        traindata.value = initialState.train;
+        await window.arweaveWallet.dispatch(transaction);
+        await anyoneCanTrain.train([clone]);
+        results.value = clone;
+    };
 </script>
